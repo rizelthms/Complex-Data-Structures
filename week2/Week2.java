67edbcbdb93be1eece5832e1fee145dc346dcf34
Rizelthomas <497836@student.saxion.nl>
2022-12-21 16:18:55 +0100

    Updated week 2 to use a hashmap instead of map and added a function so now data can be read into the hashmap

--- week2/Week2.java
+++ week2/Week2.java
@@ -1,20 +1,22 @@
 package week2;
 
 import utility.Client;
-import utility.ClientList;
 import utility.Package;
-import utility.PackageList;
-import week1.Week1;
+import week1.*;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
 
 public class Week2 {
-    static final Map<Integer, Client> clientHashMap = new HashMap<>();
-    static final Map<Integer, Package> packageHashMap = new HashMap<>();
+    static HashMap<Integer, Client> clientHashMap;
+    static HashMap<Integer, Package> packageHashMap;
+    static final String CLIENTS_FILE = "CDS_code/resources/Clients.csv";
+    static final String PACKAGES_FILE = "CDS_code/resources/Packages.csv";
 
     public static void main(String[] args) {
         final Scanner scan = new Scanner(System.in);
@@ -24,17 +26,20 @@ public class Week2 {
         int inPackageId = scan.nextInt();
         // Closing Scanner after the use
         scan.close();
-        List<Client> clients = Week1.readClientsFromCsv();
-        List<Package> packages = Week1.readPackagesFromCsv();
-        ClientList clientList = new ClientList(clients);
-        //int iterations = 100;
-        PackageList packageList = new PackageList(packages);
+
+        //Read data into data structures
+        LinkedList clients = Week1.readClientsFromCsv();
+        LinkedList packages = Week1.readPackagesFromCsv();
+        clientHashMap = readClientsHashMap();
+        packageHashMap = readPackagesHashMap();
         System.out.println();
+
+
         System.out.println("LINEAR SEARCH IN CLIENT");
         long startTime = System.nanoTime();
-        Package searchPackage = packageList.linearSearch(inPackageId);
+        Package searchPackage = packages.linearSearch(inPackageId);
         if (searchPackage != null) {
-            Client searchClient = clientList.linearSearch(searchPackage.client.id);
+            Client searchClient = clients.linearSearch(searchPackage.client.id);
             if (searchClient != null) {
                 System.out.println("utility.Package Id " + searchPackage.id + " is assigned to client " + searchClient.name
                         + " having id " + searchClient.id);
@@ -45,8 +50,10 @@ public class Week2 {
             System.out.println("No utility.Package Found with id " + inPackageId);
         }
         long endTime = System.nanoTime();
-        System.out.println("Duration in (ns) :" + (double) (endTime - startTime));
+        System.out.println("Duration in (ns): " + (double) (endTime - startTime));
         System.out.println();
+
+
         System.out.println("HASHMAP SEARCH");
         startTime = System.nanoTime();
         searchPackage = packageHashMap.get(inPackageId);
@@ -62,6 +69,39 @@ public class Week2 {
             System.out.println("No utility.Package Found with id " + inPackageId);
         }
         endTime = System.nanoTime();
-        System.out.println("Duration in (ns) :" + (double) (endTime - startTime));
+        System.out.println("Duration in (ns): " + (double) (endTime - startTime));
     }
+
+    public static HashMap readClientsHashMap() {
+        HashMap<Integer, Client> clientHashMap = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(CLIENTS_FILE))) {
+            br.readLine(); // Skip the header
+            String line;
+            while ((line = br.readLine()) != null) {
+                List<String> parts = Week1.getRecordFromLine(line);
+                Client myClient = Client.fromCsvLine(parts);
+                clientHashMap.put(myClient.id, myClient);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return clientHashMap;
+    }
+
+    public static HashMap readPackagesHashMap(){
+        HashMap<Integer, Package> packageHashMap = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(PACKAGES_FILE))) {
+            br.readLine(); // Skip the header
+            String line;
+            while ((line = br.readLine()) != null) {
+                List<String> parts = Week1.getRecordFromLine(line);
+                Package myPackage = Package.fromCsvLine(parts);
+                packageHashMap.put(myPackage.id, myPackage);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return packageHashMap;
+    }
+
 }
