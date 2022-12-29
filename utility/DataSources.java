da0b4950f18d359c5f69992c0b37126224b0fa2b
Rizelthomas <497836@student.saxion.nl>
2022-12-29 09:55:46 +0100

    Populated datasources and inputs class

--- utility/DataSources.java
+++ utility/DataSources.java
@@ -1,4 +1,24 @@
 package utility;
 
-public class DataSources {
+// Enum to represent the different data sources that can be read from CSV files
+public enum DataSources {
+    // Data sources for clients and packages
+    CLIENTS(Client.class),
+    PACKAGES(Package.class),
+    // Test data sources for clients and packages
+    TESTCLIENTS(Client.class),
+    TESTPACKAGES(Package.class);
+
+    // The type of object that the data source represents (e.g. Client or Package)
+    private final Class<?> type;
+
+    // Constructor to store the type of object for the data source
+    private DataSources(Class<?> type) {
+        this.type = type;
+    }
+
+    // Getter method for the type of object for the data source
+    public Class<?> getType(){
+        return this.type;
+    }
 }
--- utility/Inputs.java
+++ utility/Inputs.java
@@ -1,4 +1,71 @@
 package utility;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
 public class Inputs {
+    // Constants for file names and CSV delimiter
+    static final String CLIENTS_FILE = "CLIENTS.csv";
+    static final String PACKAGES_FILE = "PACKAGES.csv";
+    static final String CSV_DELIMITER = ";";
+
+    // Reads a CSV file and returns a list of objects
+    public static List<Object> readCSV(DataSources source) {
+        // Construct the file path based on the DataSources enum value
+        String filename = "/resources/" + source.toString() + ".csv";
+        System.out.println("Reading from: " + filename);
+        URL resource = Inputs.class.getResource(filename);
+        List<Object> elements = new ArrayList<>();
+
+        // Check if the file exists
+        if (resource == null) {
+            System.err.println("Warning: Could not find " + filename);
+            return elements;
+        }
+
+        // Read the file and create objects from each line
+        try (BufferedReader br = new BufferedReader(new FileReader(URLDecoder.decode(resource.getPath(), StandardCharsets.UTF_8)))) {
+            br.readLine(); // Skip the header
+            String line;
+            while ((line = br.readLine()) != null) {
+                List<String> parts = getRecordFromLine(line);
+                Object element = null;
+
+                // Create the appropriate object based on the DataSources enum value
+                switch (source) {
+                    case CLIENTS -> element = Client.fromCsvLine(parts);
+                    case PACKAGES -> element = Package.fromCsvLine(parts);
+                    case TESTCLIENTS -> element = Client.fromCsvLine(parts);
+                    case TESTPACKAGES -> element = Package.fromCsvLine(parts);
+                    default -> {
+                    }
+                }
+                // Add the object to the list
+                elements.add(element);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Read " + elements.size() + " items from " + filename);
+        return elements;
+    }
+
+    // Parses a single line from a CSV file and returns a list of values
+    public static List<String> getRecordFromLine(String line) {
+        List<String> values = new ArrayList<>();
+        try (Scanner rowScanner = new Scanner(line)) {
+            // Use the CSV delimiter to split the line into values
+            rowScanner.useDelimiter(CSV_DELIMITER);
+            while (rowScanner.hasNext()) {
+                values.add(rowScanner.next());
+            }
+        }
+        return values;
+    }
 }
